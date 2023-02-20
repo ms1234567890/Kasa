544bc5b581d879c83c278adbb905d233e5755cb4
Martina Stella <ms@MacBook-Pro-de-Martina.local>
2023-02-20 20:40:56 +0100

    ajout fichiers

---
+++ src/pages/Product.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Product = () => {
+  return (
+    <div>
+      <Navigation />
+      Accueil
+    </div>
+  )
+}
+
+export default Product;
